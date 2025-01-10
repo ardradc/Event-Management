c1c6b64146b49e2ab7804fd3a3ade3a838c29d36
Binu Paul <binspaul@gmail.com>
2025-01-10 21:42:37 +0530

    Design changes

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,32 +1,45 @@
 import {Link} from 'react-router';
-import { MenuIcon } from './Icon';
+import { DashboardIcon, ServicesIcon, UsersIcon, VendorsIcon, OrdersIcon } from './Icon';
 
 const Sidebar = () => {
     return (
-      <div className="bg-dark text-white position-fixed" style={{ width: "250px", height: "100vh", marginTop: "56px" }}>
-        <ul className="nav flex-column">
-            <li className="nav-item">
-                <Link to={'/dashboard'} className="nav-link text-white">
-                    <i className="fs-4 bi-house"><MenuIcon/></i> <span className="ms-1 d-none d-sm-inline">Dashboard</span>
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link to={'/dashboard/services'} className="nav-link text-white">
-                    <i className="fs-4 bi-house"><MenuIcon/></i> <span className="ms-1 d-none d-sm-inline">Services</span>
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link to={'/dashboard/vendors'} className="nav-link text-white">
-                    <i className="fs-4 bi-house"><MenuIcon/></i> <span className="ms-1 d-none d-sm-inline">Vendors</span>
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link to={'/dashboard/users'} className="nav-link text-white">
-                    <i className="fs-4 bi-house"><MenuIcon/></i><span className="ms-1 d-none d-sm-inline">Users</span>
-                </Link>
-            </li>
-        </ul>
-      </div>
+        <>
+            <div className="bg-dark text-white position-fixed" style={{ width: "250px", height: "100vh", marginTop: "56px" }}>
+                <ul className="nav flex-column">
+                    <li className="nav-item">
+                        <Link to={'/dashboard'} className="nav-link text-white">
+                            <i className="fs-4 bi-house"><DashboardIcon /></i> <span className="ms-1 d-none d-sm-inline">Dashboard</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <hr/>
+                    </li>
+                    <li className="nav-item">
+                        <Link to={'/dashboard/services'} className="nav-link text-white">
+                            <i className="fs-4 bi-house"><ServicesIcon /></i> <span className="ms-1 d-none d-sm-inline">Services</span>
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to={'/dashboard/vendors'} className="nav-link text-white">
+                            <i className="fs-4 bi-house"><VendorsIcon /></i> <span className="ms-1 d-none d-sm-inline">Vendors</span>
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to={'/dashboard/users'} className="nav-link text-white">
+                            <i className="fs-4 bi-house"><UsersIcon /></i><span className="ms-1 d-none d-sm-inline">Users</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <hr/>
+                    </li>
+                    <li className="nav-item">
+                        <Link to={'/dashboard/orders'} className="nav-link text-white">
+                            <i className="fs-4 bi-house"><OrdersIcon /></i><span className="ms-1 d-none d-sm-inline">Orders</span>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </>
     );
   };
 
--- src/dashboard/Tiles.jsx
+++ src/dashboard/Tiles.jsx
@@ -1,29 +1,35 @@
+import { OrdersIcon, ServicesIcon, UsersIcon, VendorsIcon } from '../Icon';
+
 const Tiles = () => {
   return (
     <div className="container mt-5">
       <div className="row">
         <div className="col-md-6 col-lg-3 mb-4">
           <div className="card shadow-sm p-3 text-center">
-            <h5 className="card-title">Users</h5>
-            <p className="card-text">Total users: 20</p>
+            <h5 className="card-title"><UsersIcon/></h5>
+            <p className="card-text">Users</p>
+            <p className="card-text">20</p>
           </div>
         </div>
         <div className="col-md-6 col-lg-3 mb-4">
           <div className="card shadow-sm p-3 text-center">
-            <h5 className="card-title">Vendors</h5>
-            <p className="card-text">Total vendors: 20</p>
+            <h5 className="card-title"><VendorsIcon/></h5>
+            <p className="card-text">Vendors</p>
+            <p className="card-text">20</p>
           </div>
         </div>
         <div className="col-md-6 col-lg-3 mb-4">
           <div className="card shadow-sm p-3 text-center">
-            <h5 className="card-title">Services</h5>
-            <p className="card-text">Total services: 20</p>
+            <h5 className="card-title"><ServicesIcon/></h5>
+            <p className="card-text">Services</p>
+            <p className="card-text">20</p>
           </div>
         </div>
         <div className="col-md-6 col-lg-3 mb-4">
           <div className="card shadow-sm p-3 text-center">
-            <h5 className="card-title">Orders</h5>
-            <p className="card-text">Total orders: 0</p>
+            <h5 className="card-title"><OrdersIcon/></h5>
+            <p className="card-text">Orders</p>
+            <p className="card-text">20</p>
           </div>
         </div>
       </div>
